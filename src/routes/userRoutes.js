9fe8546abba0de6269bfa8b3616b96f5fb064707
Lukas Ariel <130007817+lukasariel112@users.noreply.github.com>
2024-08-14 00:11:12 -0300

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,19 +2,19 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-// Criar um novo usuário
+// Aqui cria um novo usuário Lembrar de usar o metodo POST
 router.post('/users', userController.createUser);
 
-// Listar todos os usuários
+// Mostra os usuarios ja criados lembrar de usar o GET
 router.get('/users', userController.getAllUsers);
 
-// Obter detalhes de um usuário específico
+// Mostrar somente um usuário específico pode ser pelo ID
 router.get('/users/:id', userController.getUserById);
 
-// Atualizar informações de um usuário
+// Atualizar informações de um usuário lembrar pelo ID
 router.put('/users/:id', userController.updateUser);
 
-// Excluir um usuário
+// Excluir um usuário pelo ID e metodo DELETE
 router.delete('/users/:id', userController.deleteUser);
 
 module.exports = router;
