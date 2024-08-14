61bb3f9bf9de8a8483ad593d7565c4dc7485529f
Lukas Ariel <130007817+lukasariel112@users.noreply.github.com>
2024-08-14 00:12:42 -0300

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+const port = 3000;
+
+// Corpo arq JSON
+app.use(express.json());
+
+// Rotas
+app.use('/api', userRoutes);
+
+// Iniciar o servidor aqui sempre puxa o 3000
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
